dddcc4fd8106e74f6c8bd13f0c28ce64a1130b1b
arisvogis76 <arisvogis76@gmail.com>
2026-03-22 12:08:14 +0200

    Update Auto Increment.sql

--- Auto Increment.sql
+++ Auto Increment.sql
@@ -1,6 +1,10 @@
-CREATE TABLE unique_cats (
+CREATE TABLE unique_cats3 (
 cat_id INT auto_increment,
 name VARCHAR(100),
-age int
- PRIMARY KEY(cat_id)
+age int,
+ PRIMARY KEY (cat_id)
  );
+ 
+INSERT INTO unique_cats3 (name,age)
+VALUES ('bingo',5);
+select * FROM UNIQUE_CATS3;
