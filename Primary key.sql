a53a3dfb85de9ecf6e564486e7b6a7ecf13df380
arisvogis76 <arisvogis76@gmail.com>
2026-03-22 12:01:58 +0200

    Update Primary key.sql

--- Primary key.sql
+++ Primary key.sql
@@ -2,6 +2,14 @@ CREATE TABLE unique_cats (
 cat_id INT NOT NULL PRIMARY KEY,
 name VARCHAR(100),
 age int);
+
+CREATE TABLE unique_cats (
+cat_id INT NOT NULL,
+name VARCHAR(100),
+age int
+ PRIMARY KEY(cat_id)
+ );
+
 INSERT INTO unique_cats (cat_id,name,age)
 VALUES (1,'bingo',2);
 
