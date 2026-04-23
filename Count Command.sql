0d3231e6305397ff24e347018376e0dc28aea726
arisvogis76 <arisvogis76@gmail.com>
2026-04-23 11:21:18 +0300

    Create Count Command.sql

---
+++ Count Command.sql
@@ -0,0 +1,12 @@
+SELECT COUNT(*) FROM BOOKS;
+
+SELECT count(author_fname) FROM books;
+
+SELECT COUNT(DISTINCT author_fname) FROM books;
+
+SELECT released_year FROM BOOKS;
+
+SELECT COUNT(*) FROM books WHERE title LIKE '%the%';
+
+
+
