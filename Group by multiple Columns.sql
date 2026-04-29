c13cb2cf42435cfebe709f49ee4832a9dcdb016c
arisvogis76 <arisvogis76@gmail.com>
2026-04-29 11:12:52 +0300

    Create Group by multiple Columns.sql

---
+++ Group by multiple Columns.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT author_fname , author_lname FROM books ORDER BY author_lname;
+
+SELECT author_fname,author_lname , COUNT(*) FROM books GROUP BY author_lname,author_fname;
+
+
+SELECT CONCAT(author_fname, ' ' , author_lname) AS author, COUNT(*) FROM books
+GROUP BY author;
+
+
+
+
+
+
